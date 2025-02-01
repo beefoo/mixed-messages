7699d2a219c8bc721db4b35ffeac6c0a3512cfb2
beefoo <brian@youaremyjoy.org>
2025-02-01 17:33:05 -0500

    implement reset, next, prev

--- public/js/lib/AudioSelector.js
+++ public/js/lib/AudioSelector.js
@@ -14,6 +14,7 @@ export default class AudioSelector {
     const defaults = {
       debug: false,
       itemEl: 'item-detail',
+      item: 0,
       onSelect: (item) => console.log(item),
       selectEl: 'source-select',
       src: 'audio/manifest.json',
@@ -27,16 +28,24 @@ export default class AudioSelector {
     const response = await fetch(options.src);
     const table = await response.json();
     this.items = parseTable(table);
+    this.itemCount = this.items.length;
     this.$selector = document.getElementById(options.selectEl);
     this.$item = document.getElementById(options.itemEl);
-    this.index = 0;
+    this.index = options.item;
     this.loadUI(this.index);
     this.select(this.index);
     this.loadListeners();
   }
 
   loadListeners() {
+    const $next = document.getElementById('button-next');
+    const $prev = document.getElementById('button-prev');
+    const $reset = document.getElementById('button-reset');
+
     this.$selector.onchange = (_event) => this.onSelect();
+    $next.onclick = (_event) => this.next();
+    $prev.onclick = (_event) => this.prev();
+    $reset.onclick = (_event) => this.reset();
   }
 
   loadUI(selectedIndex = 0) {
@@ -48,12 +57,31 @@ export default class AudioSelector {
     this.$selector.innerHTML = html;
   }
 
+  next() {
+    let newIndex = this.index + 1;
+    if (newIndex >= this.itemCount) newIndex = 0;
+    this.$selector.selectedIndex = newIndex;
+    this.$selector.onchange();
+  }
+
   onSelect() {
     const index = parseInt(this.$selector.value, 10);
     this.select(index);
   }
 
+  prev() {
+    let newIndex = this.index - 1;
+    if (newIndex < 0) newIndex = this.itemCount - 1;
+    this.$selector.selectedIndex = newIndex;
+    this.$selector.onchange();
+  }
+
+  reset() {
+    this.select(this.index);
+  }
+
   select(index) {
+    this.index = index;
     const item = this.items[index];
     const html = `by <a href="${item.url}" target="_blank">${item.speakers}</a>`;
     this.$item.innerHTML = html;
